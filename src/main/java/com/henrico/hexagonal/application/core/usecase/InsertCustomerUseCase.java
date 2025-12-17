33af1b9dbb1aeea188bacd14ebadf7623807da68
Henrico Birochi <henrico.birochi@gmail.com>
2025-12-17 10:27:07 -0300

    config: Initial configuration and implementation of the project

---
+++ src/main/java/com/henrico/hexagonal/application/core/usecase/InsertCustomerUseCase.java
@@ -0,0 +1,11 @@
+package com.henrico.hexagonal.application.core.usecase;
+
+import com.henrico.hexagonal.application.core.domain.Customer;
+
+public class InsertCustomerUseCase {
+
+    public void insert(Customer customer, String zipCode) {
+
+    }
+
+}
